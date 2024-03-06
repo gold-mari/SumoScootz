181de85c1f923f7411c40e3385c6ffc3b56a00a9
Dylan Mahler <dmahler@ucsc.edu>
2024-03-06 12:21:01 -0800

    first commit

---
+++ src/Scenes/VersusPlay.js
@@ -0,0 +1,21 @@
+class VersusPlay extends Phaser.Scene {
+    constructor() {
+        super('versusPlayScene')
+    }
+
+    init() {
+        
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+        
+    }
+
+    update() {
+        
+    }
+}
