f8ed5aecdd0efbfe65d1dc3905765291a3d12d59
Dylan Mahler <dmahler@ucsc.edu>
2024-03-06 18:11:10 -0800

    Drivers can move!

--- src/Prefabs/Driver.js
+++ src/Prefabs/Driver.js
@@ -1,4 +1,4 @@
-class Driver extends Phaser.GameObjects.Sprite {
+class Driver extends Phaser.Physics.Arcade.Sprite {
     constructor(ID, scene, x, y, texture, frame, controls) {
         super(scene, x, y, texture, frame);
 
@@ -7,20 +7,32 @@ class Driver extends Phaser.GameObjects.Sprite {
         scene.add.existing(this);           // add object to existing, displayList, updateList
         scene.physics.add.existing(this);   // add to physics
 
+
+        // Controls ===================
         this.leftKey = controls.leftKey;
         this.rightKey = controls.rightKey;
         this.upKey = controls.upKey;
         this.downKey = controls.downKey;
         this.upshiftKey = controls.upshiftKey;
         this.downshiftKey = controls.downshiftKey;
+
+        // Direction ==================
+        this.direction = new Phaser.Math.Vector2(0);
     }
 
     update() {
-        if (Phaser.Input.Keyboard.JustDown(this.leftKey)) console.log(`${this.ID} left`);
-        if (Phaser.Input.Keyboard.JustDown(this.rightKey)) console.log(`${this.ID} right`);
-        if (Phaser.Input.Keyboard.JustDown(this.upKey)) console.log(`${this.ID} up`);
-        if (Phaser.Input.Keyboard.JustDown(this.downKey)) console.log(`${this.ID} down`);
-        if (Phaser.Input.Keyboard.JustDown(this.upshiftKey)) console.log(`${this.ID} upshift`);
-        if (Phaser.Input.Keyboard.JustDown(this.downshiftKey)) console.log(`${this.ID} downshift`);
+        // MOVEMENT ===========================================================
+        this.direction = new Phaser.Math.Vector2(0);
+
+        if (this.leftKey.isDown)    this.direction.x -= 1;
+        if (this.rightKey.isDown)  this.direction.x += 1;
+        if (this.upKey.isDown)     this.direction.y -= 1;
+        if (this.downKey.isDown)   this.direction.y += 1;
+
+        this.direction.normalize()
+        this.setVelocity(250 * this.direction.x, 250 * this.direction.y)
+
+        // if (Phaser.Input.Keyboard.JustDown(this.upshiftKey)) console.log(`${this.ID} upshift`);
+        // if (Phaser.Input.Keyboard.JustDown(this.downshiftKey)) console.log(`${this.ID} downshift`);
     }
 }
